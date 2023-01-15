88f70437583cfd012c203a0f4176c12f54c14d6b
Mariia Makarkina <mariiamak25@gmail.com>
2023-01-15 15:14:46 +0200

    add html components on page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+
+import Profile from "./Profile/Profile";
+import Statistics from "./Statistics/Statistics";
+import Friendlist from "./FriendList/Friendlist";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+
+  
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile />
+      <Statistics />
+      <Friendlist />
+      <TransactionHistory/>
+
+      </>
   );
 };
---
+++ src/components/FriendList/Friendlist.jsx
@@ -0,0 +1,11 @@
+import "./friendlist.css"
+
+const Friendlist = () => {
+  return(
+   <ul class="friend-list">
+  {/* <!-- Произвольное кол-во FriendListItem --> */}
+</ul>
+    )
+}
+
+export default Friendlist;
